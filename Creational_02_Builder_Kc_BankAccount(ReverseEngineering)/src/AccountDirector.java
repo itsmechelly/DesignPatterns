808e240eae51b10cb5fd1214b411a4ced686fa3e
Chelly izraelov <hlly.lov@gmail.com>
2021-12-17 14:54:23 +0200

    account builder

---
+++ Creational_02_Builder_Kc_BankAccount(ReverseEngineering)/src/AccountDirector.java
@@ -0,0 +1,21 @@
+
+public class AccountDirector{
+	
+	private AccountBuilder builder;
+
+	public void setAccountBuilder(AccountBuilder builder){
+		this.builder=builder;
+		}
+	
+	public Account getAccount(){
+		return builder.getAccount(); 
+		}
+
+	public void constructAccount(){
+		builder.createNewAccount();
+		builder.setId();
+		builder.setType();
+		builder.setCommission();
+		builder.setStartDate();
+	}
+}
---
+++ Creational_02_Builder_Kc_BankAccount(ReverseEngineering)/src/Test.java
@@ -0,0 +1,22 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+
+	//Builders
+	GoldAccountBuilder g = new GoldAccountBuilder();
+	PlatinumAccountBuilder p = new PlatinumAccountBuilder();
+	RegularAccountBuilder r = new RegularAccountBuilder();
+	
+	//Setup
+	createAndPrintAccount(r);
+	}
+	
+	public static void createAndPrintAccount(AccountBuilder builder){
+		AccountDirector director=new AccountDirector();
+		director.setAccountBuilder(builder);
+		director.constructAccount();
+		Account account = director.getAccount();
+		System.out.println(account);
+	}
+}
